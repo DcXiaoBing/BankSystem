7a3cc9f41f18cf8863fa28f766a9135c771b9667
chrisjhuang <cjhuang@bu.edu>
2020-05-06 05:30:24 +0900

    Update StockPosition.java

--- src/StockPosition.java
+++ src/StockPosition.java
@@ -1,21 +1,36 @@
 public class StockPosition{
 	//class to handle buy and sell orders in our atm
 	private int id;
-	private double rate; // rate to USD
-	StockPosition(String xname, double xrate){
-		this.name = xname;
-		this.rate = xrate;
+	private char position; //'S' for sell, 'B' for buy
+	private int date; 
+	private int stockId;
+	StockPosition(int xid, char xposition, int date, int stockId){
+		this.id = xid;
+		this.position = xposition;
 	}
-	public String getname() {
-		return name;
+	public int getid() {
+		return id;
 	}
-	public void setname(String xname) {
-		name = xname;
+	public void setid(int xid) {
+		id = xid;
 	}
-	public double getrate() {
-		return rate;
+	public int getstockId() {
+		return id;
 	}
-	public void setrate(double xrate) {
-		rate = xrate;
+	public void setstockId(int xstockId) {
+		stockId = xstockId;
+	}
+	public char getposition() {
+		return position;
+	}
+	public void setposition(char xposition) {
+		position = xposition;
+	}
+	
+	public int getdate() {
+		return date;
+	}
+	public void setdate(int xdate) {
+		date = xdate;
 	}
 }
